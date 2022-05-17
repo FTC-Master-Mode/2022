ea367475a22635eb41ed03e8795b30ee3b14c97c
Noah Blaut <Noah.Blaut@outlook.com>
2022-05-16 20:28:25 -0500

    teaching noah bc dum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic_OpMode.java
@@ -3,19 +3,30 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.basic_hardware;
 
 @TeleOp(name = "Basic OpMode", group = "OpMode")
 public class basic_OpMode extends OpMode {
     private final ElapsedTime runtime = new ElapsedTime();
     basic_hardware robot = new basic_hardware();
+    public double topPower = 0.0;
+    public double bottomPower = 0.0;
+    public double xAxis = gamepad1.left_stick_x;
+    public double yAxis = gamepad1.left_stick_y;
 
     @Override
-    public void init() {
-
+    public void init(){
+        robot.init();
     }
 
     @Override
     public void loop() {
-
+        if (yAxis != 0) {
+            topPower = yAxis;
+            bottomPower = yAxis - .02;
+        }
+        robot.topMotor.setPower(topPower);
+        robot.bottomMotor.setPower(bottomPower);
     }
-}
+ }
+//make so joystick does the thing when joystick move yunno liek yes :)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic_hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basic_hardware.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class basic_hardware{
-
+    public DcMotor bottomMotor = null;
+    public DcMotor topMotor = null;
+    public HardwareMap ahwMap;
     public void init() {
+        topMotor = ahwMap.get(DcMotor.class, "topMotor");
+        bottomMotor = ahwMap.get(DcMotor.class, "bottomMotor");
 
     }
 }
+
